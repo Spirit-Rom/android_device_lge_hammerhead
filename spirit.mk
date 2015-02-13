b56c70e72075372c208c37f6fa3f26f80151c590
Nico60 <ndhouailly@gmail.com>
2015-02-13 12:08:32 +0100

    Revert "hammerhead: Use halfres boot animation"
    This reverts commit eef5c9dfba244869d056756d2bc3e8d56c510922.

--- spirit.mk
+++ spirit.mk
@@ -1,7 +1,6 @@
 # Boot animation
 TARGET_SCREEN_HEIGHT := 1920
 TARGET_SCREEN_WIDTH := 1080
-TARGET_BOOTANIMATION_HALF_RES := true
 
 # Inherit some common SR stuff.
 $(call inherit-product, vendor/spirit/config/common_phone.mk)
